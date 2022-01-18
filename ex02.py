2f93f22d8821ebdaeb91b43604cf6edf3f394352
kimmingye <dusgml3779@gmail.com>
2022-01-18 23:26:37 +0900

    python start

---
+++ ex02.py
@@ -0,0 +1,11 @@
+import math
+
+name = input('Name:')
+age = eval(input('Age:'))
+print('{}의 나이는 {} 입니다.'.format(name,age))
+
+print(f'{name}의 나이는 {age} 입니다.')
+
+print(math.pi)
+print('{:.2f}'.format(math.pi))
+print(f'{math.pi:.2f}')
--- input.py
+++ input.py
@@ -1,3 +1,7 @@
-dan =5
+dan =input('Enter Number')
+
 for i in range(1,10):
-  print(dan,'x',i,'=',dan*i)
+  print(dan,'x',i,'=',int(dan)*i)
+  
+#eval함수를 쓰면 스트링 형식의 명령어를 그대로 해석하지 않고 명령어가 실행되게 해준다
+eval('print(10)')
